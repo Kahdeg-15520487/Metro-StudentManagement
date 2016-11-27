5734f2464365ecc1fbac06f36264e80057159067
KaelPiS <piskael03@gmail.com>
2016-11-27 11:48:35 +0700

    Create IntroView

--- StudentManagement/StudentManagement/ViewModel/AcademicViewModel.cs
+++ StudentManagement/StudentManagement/ViewModel/AcademicViewModel.cs
@@ -12,63 +12,68 @@ namespace StudentManagement.ViewModel
     public class AcademicViewModel : ViewModelBase
     {
 
-        //StudentDBEntities ST = new StudentDBEntities();
-        //public ICommand DetailCommand { get; set; }
-        //private string selectedSubjectID = string.Empty;
-        //public AcademicViewModel()
-        //{
-        //    DetailCommand = new RelayCommand<DataGrid>((p) => true, (p) =>
-        //    {
+        StudentDBEntities ST = new StudentDBEntities();
+        public ICommand DetailCommand { get; set; }
+        private string selectedSubjectID = string.Empty;
+        public AcademicViewModel()
+        {
+            DetailCommand = new RelayCommand<DataGrid>((p) => true, (p) =>
+            {
 
-        //        GetAcademic_Result data = (GetAcademic_Result)p.SelectedItem;
-        //        selectedSubjectID = data.SubjectID;
-        //        SubjectDetail = new ObservableCollection<object>(ST.GetDetailSubjectByID(selectedSubjectID).ToList());
-        //        SubjectDetail.FirstOrDefault();
-
-
-
-        //    });
-        //}
-        //private ObservableCollection<object> _AcademicList;
-        //public ObservableCollection<object> AcademicList
-        //{
-        //    get
-        //    {
-        //        if (_AcademicList == null)
-        //        {
-        //            _AcademicList = new ObservableCollection<object>(ST.GetAcademic().ToList());
-        //        }
-        //        return _AcademicList;
-        //    }
-        //    set
-        //    {
-        //        if (_AcademicList != value)
-        //        {
-        //            _AcademicList = value; OnPropertyChanged("AcademicList");
-        //        }
-        //    }
-        //}
+                GetAcademicByID_Result data = (GetAcademicByID_Result)p.SelectedItem;
+                selectedSubjectID = data.DisciplineID;
+                SubjectDetail = new ObservableCollection<object>(ST.GetDetailDisciplineByID(selectedSubjectID).ToList());
+                SubjectDetail.FirstOrDefault();
 
 
-        //private ObservableCollection<object> _SubjectDetail;
-        //public ObservableCollection<object> SubjectDetail
-        //{
-        //    get
-        //    {
-        //        if (_SubjectDetail == null)
-        //        {
-        //            _SubjectDetail = new ObservableCollection<object>();
-        //        }
-        //        return _SubjectDetail;
-        //    }
-        //    set
-        //    {
-        //        if (_SubjectDetail != value)
-        //        {
-        //            _SubjectDetail = value; OnPropertyChanged("SubjectDetail");
-        //        }
-        //    }
-        //}
+
+            });
+        }
+        private ObservableCollection<object> _AcademicList;
+        public ObservableCollection<object> AcademicList
+        {
+            get
+            {
+                if (_AcademicList == null)
+                {
+                    var thisUser = DialogLogginViewModel.Users[0];
+                    if (thisUser.Roll==0)
+                    {
+                        _AcademicList = new ObservableCollection<object>(ST.GetAcademicByID(thisUser.ID).ToList());
+                    }
+                    
+                }
+                return _AcademicList;
+            }
+            set
+            {
+                if (_AcademicList != value)
+                {
+                    _AcademicList = value; OnPropertyChanged("AcademicList");
+                }
+            }
+        }
+
+
+        private ObservableCollection<object> _SubjectDetail;
+        public ObservableCollection<object> SubjectDetail
+        {
+            get
+            {
+                if (_SubjectDetail == null)
+                {
+                    _SubjectDetail = new ObservableCollection<object>();
+                }
+                return _SubjectDetail;
+            }
+            set
+            {
+                if (_SubjectDetail != value)
+                {
+                    _SubjectDetail = value; OnPropertyChanged("SubjectDetail");
+                }
+            }
+        }
 
 
 
