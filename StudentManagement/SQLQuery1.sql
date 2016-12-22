ae26e658e6694591f43364f81ebc8acb83e26dee
hoideptrai123 <toilahoi007@gmail.com>
2016-12-22 16:06:12 +0700

    No Commit

--- StudentManagement/SQLQuery1.sql
+++ StudentManagement/SQLQuery1.sql
@@ -1,41 +1,77 @@
-﻿
-alter TABLE SEMESTER
-ADD DateRegister smalldatetime
-
-
-update SEMESTER
-set DateRegister='2016-12-20'
-where SemesterID='01619'
-
-
-alter proc IsDateRegisters
+﻿CREATE proc IsDateRegister
 as
 begin
-
+--create cursor
 declare ListDateRegister cursor
-for select DateRegister  from Semester 
-declare @IsDate int,@DateRegister smalldatetime ,@NowDay smalldatetime
+for select SemesterID, DateRegister  from Semester 
+declare @IsDate bit,@DateRegister smalldatetime ,@NowDay smalldatetime,@SemesterID nvarchar(10),@temp nvarchar(10)
 set @NowDay=GETDATE()
+set @temp='0'
 open ListDateRegister
-FETCH NEXT FROM ListDateRegister into @DateRegister
+FETCH NEXT FROM ListDateRegister into @SemesterID, @DateRegister
 WHILE (@@FETCH_STATUS=0)
 begin
 if(@NowDay>=@DateRegister and @NowDay<=@DateRegister+3)
 begin
 set @IsDate=1
-FETCH NEXT FROM ListDateRegister into @DateRegister
-break
+set @temp=@SemesterID
 end
 else begin set @IsDate=0
-FETCH NEXT FROM ListDateRegister into @DateRegister end
+ end
+update Semester
+set IsDateRegister=@IsDate
+where DateRegister=@DateRegister
+FETCH NEXT FROM ListDateRegister into @SemesterID,@DateRegister
 end
 DEALLOCATE ListDateRegister
-return @IsDate
+if(@temp='0') set @temp=@SemesterID
+select IsDateRegister,SemesterID from Semester where SemesterID=@temp
 end
 
 
+alter TABLE SEMESTER
+ADD DateRegister smalldatetime
+
+alter table SEMESTER
+add IsDateRegister bit
+
+update SEMESTER
+set IsDateRegister=0
+
+go
+CREATE proc IsDateRegister
+as
+begin
+--create cursor
+declare ListDateRegister cursor
+for select SemesterID, DateRegister  from Semester 
+declare @IsDate bit,@DateRegister smalldatetime ,@NowDay smalldatetime,@SemesterID nvarchar(10),@temp nvarchar(10)
+set @NowDay=GETDATE()
+set @temp='0'
+open ListDateRegister
+FETCH NEXT FROM ListDateRegister into @SemesterID, @DateRegister
+WHILE (@@FETCH_STATUS=0)
+begin
+if(@NowDay>=@DateRegister and @NowDay<=@DateRegister+3)
+begin
+set @IsDate=1
+set @temp=@SemesterID
+end
+else begin set @IsDate=0
+ end
+update Semester
+set IsDateRegister=@IsDate
+where DateRegister=@DateRegister
+FETCH NEXT FROM ListDateRegister into @SemesterID,@DateRegister
+end
+DEALLOCATE ListDateRegister
+if(@temp='0') set @temp=@SemesterID
+select IsDateRegister,SemesterID from Semester where SemesterID=@temp
+end
+
+exec IsDateRegister
 
 
-declare @a int
-exec @a= IsDateRegisters
-print @a
+update Semester
+set DateRegister='2016-12-21'
+where SemesterID='01619'
